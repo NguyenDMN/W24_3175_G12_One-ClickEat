0fc7a253728a670063be64820bfbd044db42ad0f
DangMinhNguyen <nguyend78@student.douglascollege.ca>
2024-02-16 14:47:20 -0800

    Initial commit

---
+++ app/src/main/java/com/example/w24_3175_g12_one_clickeat/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.w24_3175_g12_one_clickeat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
